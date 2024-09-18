2973d7d98bd15439329e5ca219f86851d9360b6b
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2024-09-18 18:47:03 -0300

    Pequeno music player criado com HTML, CSS e Javascript

---
+++ songs.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        src: "./src/Castlevania SOTN Crystal Teardrop.mp3",
+        name: "Crystal Teardrops - Castlevania Symphony of the Night",
+        thumb: "./src/thumbnails/annie.jpg",
+    },
+    {
+        src: "./src/Castlevania SOTN Marble Gallery.mp3",
+        name: "Marble Gallery - Castlevania Symphony of the Night",
+        thumb: "./src/thumbnails/annie.jpg",
+    },
+    {
+        src: "./src/Sonic CD   Tidal Tempest Good Future JPEU HD.mp3",
+        name: "Tidal Tempest Good Future - Sonic CD",
+        thumb: "./src/thumbnails/annie.jpg",
+    },
+    {
+        src: "./src/Crepe Suzette.mp3",
+        name: "Crepe Suzette",
+        thumb: "./src/thumbnails/annie.jpg",
+    },
+];
